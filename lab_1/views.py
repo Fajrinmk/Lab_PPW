5ae569877f2c93a5e40a5a8964c566ed0b4584a4
fajrinmk <fajrinmk@gmail.com>
2017-08-31 11:04:05 +0700

    commit semua

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Enter your name here
-mhs_name = '' # TODO Implement this
+mhs_name = 'Fajrin Maulana K' # TODO Implement this
 
 # Create your views here.
 def index(request):
-    response = {'name': mhs_name}
+    response = {'name': mhs_name, "age": calculate_age(1998)}
     return render(request, 'index.html', response)
 
 # TODO Implement this to complete last checklist
 def calculate_age(birth_year):
-    pass
+    today = datetime.now().year
+    return today - birth_year
