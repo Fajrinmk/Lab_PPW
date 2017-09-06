de8d3d3737b7e055cfba495deb231a278f1ed7a4
fajrinmk <fajrinmk@gmail.com>
2017-09-06 17:05:56 +0700

    indentation error and index2 html fixed

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from datetime import datetime, date
 # Enter your name here
-mhs_name = '' # TODO Implement this
+mhs_name = 'Fajrin Maulana K.' # TODO Implement this
 curr_year = int(datetime.now().strftime("%Y"))
 birth_date = date(1998, 7, 3) #TODO Implement this, format (Year, Month, Date)
 # Create your views here.
 def index(request):
     response = {'name': mhs_name, 'age': calculate_age(birth_date.year)}
-    return render(request, 'index_lab1.html', response)
+    return render(request, 'index_lab2.html', response)
 
 def calculate_age(birth_year):
     return curr_year - birth_year if birth_year <= curr_year else 0
--- lab_2/urls.py
+++ lab_2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-    from .views import index
+from .views import index
     #url for app
-    urlpatterns = [
-        url(r'^$', index, name='index'),
-    ]
+urlpatterns = [
+    url(r'^$', index, name='index'),
+]
