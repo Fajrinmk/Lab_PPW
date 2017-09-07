27c0c887365745448d85b597eb0f99220560bbd9
fajrinmk <fajrinmk@gmail.com>
2017-09-07 09:35:32 +0700

    checklist ke 6

--- lab_2_addon/views.py
+++ lab_2_addon/views.py
@@ -5,7 +5,7 @@ from lab_1.views import mhs_name, birth_date
 #TODO Implement
 bio_dict = [{'subject' : 'Name', 'value' : mhs_name},\
 {'subject' : 'Birth Date', 'value' : birth_date.strftime('%d %B %Y')},\
-{'subject' : 'Sex', 'value' : ''}]
+{'subject' : 'Sex', 'value' : 'Male'}]
 
 def index_addon(request):
     response = {}
