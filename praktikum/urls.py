fda7bfab17f7788ed2ff9384fd913848374a24d0
fajrinmk <fajrinmk@gmail.com>
2017-09-13 13:49:30 +0700

    urls.py pada praktikum

--- praktikum/urls.py
+++ praktikum/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 import lab_1.urls as lab_1
 import lab_2.urls as lab_2
+import lab_3.urls as lab_3
 import lab_2_addon.urls as lab_2_addon
+
 from lab_1.views import index as index_lab1
 
 
@@ -27,5 +29,6 @@ urlpatterns = [
     url(r'^lab-1/', include(lab_1,namespace='lab-1')),
     url(r'^lab-2/', include(lab_2,namespace='lab-2')),
     url(r'^lab-2-addon/', include(lab_2_addon,namespace='lab_2_addon')),
+    url(r'^lab-3/', include(lab_3,namespace='lab-3')),
     url(r'^$', RedirectView.as_view(url='lab-2/', permanent = True), name='$')
 ]
