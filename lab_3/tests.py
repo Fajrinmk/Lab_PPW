d4178e9d24057292d428b2d582a00aa1854992cd
fajrinmk <fajrinmk@gmail.com>
2017-09-13 14:10:56 +0700

    semua step sudah dijalankan

--- lab_3/tests.py
+++ lab_3/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import resolve
+from .models import Diary
+from django.utils import timezone
 from .views import index
 
 class Lab3Test(TestCase):
@@ -14,3 +16,22 @@ class Lab3Test(TestCase):
     def test_lab_3_using_index_func(self):
         found = resolve('/lab-3/')
         self.assertEqual(found.func, index)
+
+    def test_model_can_create_new_activity(self):
+        #Creating a new activity
+        new_activity = Diary.objects.create(date=timezone.now(),activity='Aku mau latihan ngoding deh')
+        #Retrieving all available activity
+        counting_all_available_activity = Diary.objects.all().count()
+        self.assertEqual(counting_all_available_activity,1)
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lab-3/add_activity/', data={'date': '2017-10-12T14:14', 'activity' : 'Maen Dota Kayaknya Enak'})
+        counting_all_available_activity = Diary.objects.all().count()
+        self.assertEqual(counting_all_available_activity, 1)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lab-3/')
+
+        new_response = self.client.get('/lab-3/')
+        html_response = new_response.content.decode('utf8')
+        self.assertIn('Maen Dota Kayaknya Enak', html_response)
