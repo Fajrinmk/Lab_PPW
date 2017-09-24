2a1589b41c67add56127667ff298fe3cea4d7774
fajrinmk <fajrinmk@gmail.com>
2017-09-24 22:27:14 +0700

    step 13

--- lab_4/views.py
+++ lab_4/views.py
@@ -3,7 +3,7 @@ from lab_2.views import landing_page_content
 
 # Create your views here.
 response = {'author': "Fajrin Maulana K"} #TODO Implement yourname
-about_me = ["PJ voli", "suka burger", "suka ppw", "suka ngoding", "selalu datang ke kelas", "tidak pernah tidur di kelas"]
+about_me = []
 def index(request):
     response['content'] = landing_page_content
     html = 'lab_4/lab_4.html'
