8f3eee43965817ab3ea093d6a816d2f239484db2
fajrinmk <fajrinmk@gmail.com>
2017-09-24 22:04:22 +0700

    ayo mulai

--- lab_4/tests.py
+++ lab_4/tests.py
@@ -3,15 +3,15 @@ from django.test import Client
 from django.urls import resolve
 from django.http import HttpRequest
 from .views import index, about_me, landing_page_content
-    # Create your tests here.
+# Create your tests here.
 
 class Lab4UnitTest(TestCase):
-	def test_lab_4_url_is_exist(self):
-    	response = Client().get('/lab-4/')
+    def test_lab_4_url_is_exist(self):
+        response = Client().get('/lab-4/')
         self.assertEqual(response.status_code, 200)
 
     def test_about_me_more_than_6(self):
-        elf.assertTrue(len(about_me) >= 6)
+        self.assertTrue(len(about_me) >= 6)
 
     def test_lab4_using_index_func(self):
         found = resolve('/lab-4/')
@@ -27,4 +27,4 @@ class Lab4UnitTest(TestCase):
 
         #Chceking whether all About Me Item is rendered
         for item in about_me:
-            self.assertIn(item,html_response)      
+            self.assertIn(item,html_response)   
