0dbc32acdae47aa049abc3bb70b45b239797cd2a
fajrinmk <fajrinmk@gmail.com>
2017-09-25 11:06:58 +0700

    tambah foto

--- lab_4/models.py
+++ lab_4/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils import timezone
+import pytz
 
 class Message(models.Model):
-    name = models.CharField(max_length=27)
-    email = models.EmailField()
-    message = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+	def converTime():
+		return timezone.now() + timezone.timedelta(hours=7)
 
-    def __str__(self):
-        return self.message
+	name = models.CharField(max_length=27)
+	email = models.EmailField()
+	message = models.TextField()
+	created_date = models.DateTimeField(default=converTime)
+
+	def __str__(self):
+	    return self.message
+
+
 
