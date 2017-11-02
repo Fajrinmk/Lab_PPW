3aa110875ec987e088534dee2731665940aa281c
fajrinmk <fajrinmk@gmail.com>
2017-11-02 11:32:06 +0700

    coba biar bisa runserver

--- lab_6/views.py
+++ lab_6/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+response = {}
+def index(request):    
+    response['author'] = "Fajrin Maulana K"
+    html = 'lab_6/lab_6.html'
+    return render(request, html, response)
