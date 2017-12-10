9fbcab7ae216fc160073c82b89993d897220cedf
fajrinmk <fajrinmk@gmail.com>
2017-12-10 23:02:46 +0700

    show tabel optical dan soundcard

--- lab_9/views.py
+++ lab_9/views.py
@@ -1,52 +1,62 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-#catatan: tidak bisa menampilkan messages jika bukan menggunakan method 'render'
-from .api_enterkomputer import get_drones
+from .api_enterkomputer import (
+    get_drones, get_soundcards, get_opticals
+)
+
 
 response = {}
 
-# NOTE : untuk membantu dalam memahami tujuan dari suatu fungsi (def)
-# Silahkan jelaskan menggunakan bahasa kalian masing-masing, di bagian atas
-# sebelum fungsi tersebut.
 
-# ======================================================================== #
-# User Func
-# Apa yang dilakukan fungsi INI? #silahkan ganti ini dengan penjelasan kalian 
 def index(request):
-    print ("#==> masuk index")
+    '''
+    Jika belum login redirect ke login, jika sudah ke profile
+    '''
+
     if 'user_login' in request.session:
         return HttpResponseRedirect(reverse('lab-9:profile'))
     else:
+        # clear response cache
+        response = {}
         html = 'lab_9/session/login.html'
         return render(request, html, response)
 
+
 def set_data_for_session(res, request):
+    '''
+    Menyimpan data-data login pada session
+    '''
     response['author'] = request.session['user_login']
     response['access_token'] = request.session['access_token']
     response['kode_identitas'] = request.session['kode_identitas']
     response['role'] = request.session['role']
     response['drones'] = get_drones().json()
+    response['soundcards'] = get_soundcards().json()
+    response['opticals'] = get_opticals().json()
 
-    # print ("#drones = ", get_drones().json(), " - response = ", response['drones'])
-    ## handling agar tidak error saat pertama kali login (session kosong)
+    response['fav_drones'] = {}
     if 'drones' in request.session.keys():
         response['fav_drones'] = request.session['drones']
-    # jika tidak ditambahkan else, cache akan tetap menyimpan data
-    # sebelumnya yang ada pada response, sehingga data tidak up-to-date
-    else:
-        response['fav_drones'] = []
+
+    response['fav_soundcards'] = {}
+    if 'soundcards' in request.session.keys():
+        response['fav_soundcards'] = request.session['soundcards']
+
+    response['fav_opticals'] = {}
+    if 'opticals' in request.session.keys():
+        response['fav_opticals'] = request.session['opticals']
+
 
 def profile(request):
-    print ("#==> profile")
-    ## sol : bagaimana cara mencegah error, jika url profile langsung diakses
+    '''
+    Views untuk halaman profile
+    '''
+
+    # mencegah error, jika url profile langsung diakses
     if 'user_login' not in request.session.keys():
         return HttpResponseRedirect(reverse('lab-9:index'))
-    ## end of sol
 
     set_data_for_session(response, request)
 
@@ -55,62 +65,72 @@ def profile(request):
 
 # ======================================================================== #
 
-### Drones
-def add_session_drones(request, id):
+# Items
+
+
+def add_session_item(request, key, id):
+    # print ("#ADD session item")
     ssn_key = request.session.keys()
-    if not 'drones' in ssn_key:
-        print ("# init drones ")
-        request.session['drones'] = [id]
+    if key not in ssn_key:
+        request.session[key] = [id]
     else:
-        drones = request.session['drones']
-        print ("# existing drones => ", drones)
-        if id not in drones:
-            print ('# add new item, then save to session')
-            drones.append(id)
-            request.session['drones'] = drones
+        items = request.session[key]
+        if id not in items:
+            items.append(id)
+            request.session[key] = items
 
-    messages.success(request, "Berhasil tambah drone favorite")
+    msg = "Berhasil tambah " + key + " favorite"
+    messages.success(request, msg)
     return HttpResponseRedirect(reverse('lab-9:profile'))
 
-def del_session_drones(request, id):
-    print ("# DEL drones")
-    drones = request.session['drones']
-    print ("before = ", drones)
-    drones.remove(id) #untuk remove id tertentu dari list
-    request.session['drones'] = drones
-    print ("after = ", drones)
 
-    messages.error(request, "Berhasil hapus dari favorite")
+def del_session_item(request, key, id):
+    # print ("# DEL session item")
+    items = request.session[key]
+    # print ("before = ", items)
+    items.remove(id)
+    request.session[key] = items
+    # print ("after = ", items)
+
+    msg = "Berhasil hapus item " + key + " dari favorite"
+    messages.error(request, msg)
     return HttpResponseRedirect(reverse('lab-9:profile'))
 
-def clear_session_drones(request):
-    print ("# CLEAR session drones")
-    print ("before 1 = ", request.session['drones'])
-    del request.session['drones']
 
-    messages.error(request, "Berhasil reset favorite drones")
-    return HttpResponseRedirect(reverse('lab-9:profile'))
+def clear_session_item(request, key):
+    if key in request.session:  
+        del request.session[key]
+        msg = "Berhasil hapus session : favorite " + key
+        messages.error(request, msg)
+    return HttpResponseRedirect(reverse('lab-9:index'))
 
 # ======================================================================== #
 # COOKIES
 
-# Apa yang dilakukan fungsi INI? #silahkan ganti ini dengan penjelasan kalian 
+
 def cookie_login(request):
-    print ("#==> masuk login")
+    '''
+    Login dengan cookie, cek apakah sudah login atau belum
+    '''
+
+    # print ("#==> masuk login")
     if is_login(request):
         return HttpResponseRedirect(reverse('lab-9:cookie_profile'))
     else:
         html = 'lab_9/cookie/login.html'
         return render(request, html, response)
 
+
 def cookie_auth_login(request):
-    print ("# Auth login")
+    '''
+    Memproses login form dan set cookie jika berhasil
+    '''
+
     if request.method == "POST":
         user_login = request.POST['username']
         user_password = request.POST['password']
 
         if my_cookie_auth(user_login, user_password):
-            print ("#SET cookies")
             res = HttpResponseRedirect(reverse('lab-9:cookie_login'))
 
             res.set_cookie('user_login', user_login)
@@ -124,31 +144,41 @@ def cookie_auth_login(request):
     else:
         return HttpResponseRedirect(reverse('lab-9:cookie_login'))
 
+
 def cookie_profile(request):
-    print ("# cookie profile ")
+    '''
+    Menampilkan halaman profile dengan cookie
+    '''
+
     # method ini untuk mencegah error ketika akses URL secara langsung
     if not is_login(request):
-        print ("belum login")
         return HttpResponseRedirect(reverse('lab-9:cookie_login'))
     else:
         # print ("cookies => ", request.COOKIES)
         in_uname = request.COOKIES['user_login']
-        in_pwd= request.COOKIES['user_password']
+        in_pwd = request.COOKIES['user_password']
 
-        # jika cookie diset secara manual (usaha hacking), distop dengan cara berikut
-        # agar bisa masuk kembali, maka hapus secara manual cookies yang sudah diset
+        # jika cookie diset secara manual (usaha hacking),
+        # distop dengan cara berikut
+        # agar bisa masuk kembali, maka hapus secara
+        # manual cookies yang sudah diset
         if my_cookie_auth(in_uname, in_pwd):
             html = "lab_9/cookie/profile.html"
-            res =  render(request, html, response)
+            res = render(request, html, response)
             return res
         else:
-            print ("#login dulu")
+            # print ("#login dulu")
             msg = "Kamu tidak punya akses :P "
             messages.error(request, msg)
             html = "lab_9/cookie/login.html"
             return render(request, html, response)
 
+
 def cookie_clear(request):
+    '''
+    Clear cookie
+    '''
+
     res = HttpResponseRedirect('/lab-9/cookie/login')
     res.delete_cookie('lang')
     res.delete_cookie('user_login')
@@ -157,49 +187,22 @@ def cookie_clear(request):
     messages.info(request, msg)
     return res
 
-# Apa yang dilakukan fungsi ini?
+
 def my_cookie_auth(in_uname, in_pwd):
-    my_uname = "utest" #SILAHKAN ganti dengan USERNAME yang kalian inginkan
-    my_pwd = "ptest" #SILAHKAN ganti dengan PASSWORD yang kalian inginkan
+    '''
+    Authentikasi manual cookie, cek apakah cookie sama
+    dengan my_uname dan my_pwd
+
+    '''
+
+    my_uname = "username"  # Ganti dengan USERNAME yang kalian inginkan
+    my_pwd = "password"  # Ganti dengan PASSWORD yang kalian inginkan
     return in_uname == my_uname and in_pwd == my_pwd
 
-#Apa yang dilakukan fungsi ini? 
+
 def is_login(request):
-    return 'user_login' in request.COOKIES and 'user_password' in request.COOKIES
-
-### General Function
-def add_session_item(request, key, id):
-    print ("#ADD session item")
-    ssn_key = request.session.keys()
-    if not key in ssn_key:
-        request.session[key] = [id]
-    else:
-        items = request.session[key]
-        if id not in items:
-            items.append(id)
-            request.session[key] = items
-
-    msg = "Berhasil tambah " + key +" favorite"
-    messages.success(request, msg)
-    return HttpResponseRedirect(reverse('lab-9:profile'))
-
-def del_session_item(request, key, id):
-    print ("# DEL session item")
-    items = request.session[key]
-    print ("before = ", items)
-    items.remove(id)
-    request.session[key] = items
-    print ("after = ", items)
-
-    msg = "Berhasil hapus item " + key + " dari favorite"
-    messages.error(request, msg)
-    return HttpResponseRedirect(reverse('lab-9:profile'))
-
-def clear_session_item(request, key):
-    del request.session[key]
-    msg = "Berhasil hapus session : favorite " + key
-    messages.error(request, msg)
-    return HttpResponseRedirect(reverse('lab-9:index'))
-
-# ======================================================================== #
-
+    '''
+    Cek apakah sudah login atau belum
+    '''
+    return ('user_login' in request.COOKIES and
+            'user_password' in request.COOKIES)
